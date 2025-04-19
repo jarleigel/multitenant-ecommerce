0257bbe777d34754417e390cf2dfed3ea63fc557
Jarle <jarleigel@gmail.com>
2025-04-19 14:49:53 +0200

    02: customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,33 @@
+import { Button } from '@/components/ui/button';
+import { Checkbox } from '@/components/ui/checkbox';
+import { Input } from '@/components/ui/input';
+import { Progress } from '@/components/ui/progress';
+import { Textarea } from '@/components/ui/textarea';
 
 
 export default function Home() {
   return (
+    <div className='p-30'>
+
+   <div className='flex flex-col gap-y-4'>
    <div>
-    <p className='text-rose-500'>
-      Heisann
-    </p>
+    <Button variant="elevated">
+      Jeg er en button
+    </Button>
    </div>
+   <div>
+    <Input placeholder='Jeg er en input' />
+   </div>
+   <div>
+    <Progress value={33} />
+   </div>
+   <div>
+    <Textarea placeholder="Jeg er et tekstområde.." />
+   </div>
+   <div>
+    <Checkbox />
+   </div>
+   </div>
+    </div>
   );
 }
