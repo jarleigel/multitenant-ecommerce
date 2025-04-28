ce6842611abd17138c74e88f8ad37ae4c0dbfb1a
Jarle <jarleigel@gmail.com>
2025-04-28 22:13:24 +0200

    07: trpc integration

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,11 +1,10 @@
-import { Category } from '@/payload-types';
-import configPromise from '@payload-config'
-import { getPayload } from 'payload';
 
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
 import { Footer } from './footer'
 import {Navbar} from './navbar'
-import { SearchFilters } from './search-filters'
-import { CustomCategory } from './types';
+import { SearchFilters, SearchFiltersSkeleton } from './search-filters'
+import { getQueryClient, trpc } from '@/trpc/server';
+import { Suspense } from 'react';
 
 interface Props {
     children: React.ReactNode
@@ -13,41 +12,21 @@ interface Props {
 
 const Layout = async ({children}: Props) => {
 
-        const payload = await getPayload({
-          config: configPromise,
-        })
-
-            const data = await payload.find({
-    collection: 'categories',
-    depth: 1,
-    pagination: false,
-    where: {
-      parent: {
-        exists: false 
-      }
-    },
-    sort: "name"
-  })
-
-  const formattedData: CustomCategory[] = data.docs.map((doc) => ({
-    ...doc,
-    subcategories:( doc.subcategories?.docs ?? []).map((doc) => ({
-        // Because of "depth: 1" vi er sikker på st "doc" vil ha en type "Category"
-        ...(doc as Category),
-        subcategories: undefined,
-    }))
-  }))
-
-//   console.log({
-//     data,
-//     formattedData
-//   });
+    const queryClient = getQueryClient()
+    void queryClient.prefetchQuery(
+      trpc.categories.getMany.queryOptions()
+    )
   
 
     return (
         <div className='flex flex-col min-h-screen'>
             <Navbar />
-            <SearchFilters data={formattedData} />
+            <HydrationBoundary state={dehydrate(queryClient)}>
+            <Suspense fallback={<SearchFiltersSkeleton />}>
+
+                        <SearchFilters />
+            </Suspense>
+            </HydrationBoundary>
             <div className='flex-1 bg-[#F4F4F0]'>
 
             {children}
--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,9 +1,8 @@
-
 export default function Home() {
 
   return (
     <div>
-      HOME
+      HEIM
     </div>
   );
 }
