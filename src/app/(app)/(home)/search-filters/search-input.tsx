73422e0689d4ed23f1cff04559cdc957b907ed31
Jarle <jarleigel@gmail.com>
2025-05-02 22:10:02 +0200

    09: auth states

--- src/app/(app)/(home)/search-filters/search-input.tsx
+++ src/app/(app)/(home)/search-filters/search-input.tsx
@@ -1,9 +1,12 @@
 'use client'
 import { useState } from 'react'
-import { ListFilterIcon, SearchIcon } from 'lucide-react'
+import { BookmarkCheckIcon, ListFilterIcon, SearchIcon } from 'lucide-react'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { CategoriesSidebar } from './categories-sidebar'
+import { useTRPC } from '@/trpc/client'
+import { useQuery } from '@tanstack/react-query'
+import Link from 'next/link'
 
 interface Props {
     disabled?: boolean
@@ -13,7 +16,9 @@ export const SearchInput = ({
     disabled,
 }: Props) => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false)
-    // console.log(data);
+
+    const trpc = useTRPC()
+    const session = useQuery(trpc.auth.session.queryOptions())
     
     return (
         <div className='flex items-center gap-2 w-full'>
@@ -22,7 +27,7 @@ export const SearchInput = ({
                 <SearchIcon className='absolute left-3 top-1/2 -translate-y-1/2 size-4 text-neutral-500'/>
                 <Input className='pl-8' placeholder="Search product" disabled={disabled} />
             </div>
-            {/* TODO: Categories vie all button */}
+
             <Button
                 variant="elevated"
                 className='size-12 shrink-0 flex lg:hidden'
@@ -30,7 +35,18 @@ export const SearchInput = ({
             >
                 <ListFilterIcon />
             </Button>
-            {/* TODO: Library button */}
+            
+            {session.data?.user && (
+                <Button
+                asChild
+                variant="elevated">
+                    <Link href="/library">
+                    <BookmarkCheckIcon />
+                    Library
+                    </Link>
+
+                </Button>
+            )}
         </div>
     )
 }
