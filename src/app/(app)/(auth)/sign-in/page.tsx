8aef3475f4be0ec18017f37b903d4810de1fe486
Jarle <jarleigel@gmail.com>
2025-05-22 18:24:18 +0200

    28: deployment

--- src/app/(app)/(auth)/sign-in/page.tsx
+++ src/app/(app)/(auth)/sign-in/page.tsx
@@ -2,15 +2,15 @@ import { SignInView } from '@/modules/auth/ui/views/sign-in-view'
 import { caller } from '@/trpc/server'
 import { redirect } from 'next/navigation'
 
-const Page = async () => {
-    const session = await caller.auth.session()
+export const dynamic = 'force-dynamic'
 
-    if (session.user) {
-        redirect("/")
-    }
-    return (
-        <SignInView />
-    )
+const Page = async () => {
+  const session = await caller.auth.session()
+
+  if (session.user) {
+    redirect('/')
+  }
+  return <SignInView />
 }
 
 export default Page
