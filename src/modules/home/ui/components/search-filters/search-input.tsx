c558876870f7c6e147b666235f9d2cabe14778b0
Jarle <jarleigel@gmail.com>
2025-05-22 20:27:36 +0200

    32 search filter

--- src/modules/home/ui/components/search-filters/search-input.tsx
+++ src/modules/home/ui/components/search-filters/search-input.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { BookmarkCheckIcon, ListFilterIcon, SearchIcon } from 'lucide-react'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
@@ -7,46 +7,58 @@ import { CategoriesSidebar } from './categories-sidebar'
 import { useTRPC } from '@/trpc/client'
 import { useQuery } from '@tanstack/react-query'
 import Link from 'next/link'
+import { useProductFilters } from '@/modules/products/hooks/use-product-filters'
 
 interface Props {
-    disabled?: boolean
+  disabled?: boolean
 }
 
-export const SearchInput = ({
-    disabled,
-}: Props) => {
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+export const SearchInput = ({ disabled }: Props) => {
+  const [filters, setFilters] = useProductFilters()
+  const [searchValue, setSearchValue] = useState(filters.search)
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
 
-    const trpc = useTRPC()
-    const session = useQuery(trpc.auth.session.queryOptions())
-    
-    return (
-        <div className='flex items-center gap-2 w-full'>
-            <CategoriesSidebar open={isSidebarOpen} onOpenChange={setIsSidebarOpen} />
-            <div className='relative w-full'>
-                <SearchIcon className='absolute left-3 top-1/2 -translate-y-1/2 size-4 text-neutral-500'/>
-                <Input className='pl-8' placeholder="Search product" disabled={disabled} />
-            </div>
+  const trpc = useTRPC()
+  const session = useQuery(trpc.auth.session.queryOptions())
 
-            <Button
-                variant="elevated"
-                className='size-12 shrink-0 flex lg:hidden'
-                onClick={() => setIsSidebarOpen(true)}
-            >
-                <ListFilterIcon />
-            </Button>
-            
-            {session.data?.user && (
-                <Button
-                asChild
-                variant="elevated">
-                    <Link href="/library">
-                    <BookmarkCheckIcon />
-                    Library
-                    </Link>
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setFilters({ search: searchValue })
+    }, 400)
 
-                </Button>
-            )}
-        </div>
-    )
+    return () => clearTimeout(timeoutId)
+  }, [searchValue, setFilters])
+
+  return (
+    <div className="flex items-center gap-2 w-full">
+      <CategoriesSidebar open={isSidebarOpen} onOpenChange={setIsSidebarOpen} />
+      <div className="relative w-full">
+        <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 size-4 text-neutral-500" />
+        <Input
+          value={searchValue}
+          className="pl-8"
+          placeholder="Search product"
+          disabled={disabled}
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
+      </div>
+
+      <Button
+        variant="elevated"
+        className="size-12 shrink-0 flex lg:hidden"
+        onClick={() => setIsSidebarOpen(true)}
+      >
+        <ListFilterIcon />
+      </Button>
+
+      {session.data?.user && (
+        <Button asChild variant="elevated">
+          <Link href="/library">
+            <BookmarkCheckIcon />
+            Library
+          </Link>
+        </Button>
+      )}
+    </div>
+  )
 }
